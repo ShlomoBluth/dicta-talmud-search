602c651a23c83b260be1938818db38283c04a226
Shlomo Bluth <lomibluth@gmail.com>
2021-03-24 11:23:05 +0200

    add commit

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -21,3 +21,13 @@ import '../../searchShared/searchCommands.js'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+import addContext from 'mochawesome/addContext'
+
+Cypress.on("test:after:run", (test, runnable) => {
+    
+    let videoName = Cypress.spec.name
+    videoName = videoName.replace('/.js.*', '.js')
+    const videoUrl = 'videos/' + videoName + '.mp4'
+
+    addContext({ test }, videoUrl)
+});
